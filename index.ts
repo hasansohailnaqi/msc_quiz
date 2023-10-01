7fc1637da2932444fb9055f0d486a4ab408ce8fc
hasansohailnaqi <hasansohail793@gmail.com>
2023-10-02 01:06:14 +0500

    Initial commit

---
+++ index.ts
@@ -0,0 +1,58 @@
+
+import chalk from "chalk";
+import inquirer from "inquirer";
+
+
+/*
+
+let a = await inquirer.prompt({
+    type: "input",
+    name: "msc",
+    message: "what is your name",
+})
+
+console.log(chalk.green(a.msc));
+
+*/
+
+const apiLink:string = "https://opentdb.com/api.php?amount=5&category=18&difficulty=easy&type=multiple";
+
+let fetchData = async (data:string) => {
+    let fetchQuiz:any = await fetch(data)
+    let res = await fetchQuiz.json()
+    return res.results;
+}
+
+let data = await fetchData(apiLink);
+
+let startQuiz = async () => {
+    let score:number = 0
+    let name = await inquirer.prompt({
+        type: "input",
+        name: "msc",
+        message: "what is your name?",
+    })
+for(let i = 1; i<5; i++){
+    let  answers =  [...data[i].  incorrect_answers, data[i].correct_answer];
+
+    let ans = await inquirer.prompt({
+        type: "list",
+        name: "quiz",
+        message: data[i].question,
+        choices: answers.map((val:any)=> val),
+
+    });
+
+    if(ans.quiz == data[i].correct_answer){
+        ++score;
+        console.log(chalk.bold.italic.blue("correct"))
+
+    } else{
+        console.log(`correct answer is ${chalk.bold.italic.red(data[i].correct_answer)}`)
+    }
+}
+console.log(`dear ${chalk.green.bold(name.msc)}, your score is ${chalk.red.bold(score)} out of ${chalk.red.bold('5')}`);
+
+};
+
+startQuiz();
